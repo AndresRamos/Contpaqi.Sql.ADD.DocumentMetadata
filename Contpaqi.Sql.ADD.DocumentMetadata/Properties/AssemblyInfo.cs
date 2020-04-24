9c596bdda5ea858acbf862a9d7812853522d35a5
Andres Ramos <andres@arsoft.net>
2020-04-24 15:07:42 -0500

    Add project files.

---
+++ Contpaqi.Sql.ADD.DocumentMetadata/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Contpaqi.Sql.ADD.DocumentMetadata")]
+[assembly: AssemblyDescription("Modelo de la base de datos Document Metadata del ADD")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AR Software")]
+[assembly: AssemblyProduct("Contpaqi.Sql.ADD.DocumentMetadata")]
+[assembly: AssemblyCopyright("Copyright © AR Software 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("452e0a04-79d9-4ffa-9e2c-ed6c20d85ab3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
